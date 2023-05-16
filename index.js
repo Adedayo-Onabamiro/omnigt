971eb194676b4cb8b4f2e8d2e5faad775746d573
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-05-16 15:22:06 +0100

    complete work 1.0 ready

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+// navigation bar code start
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
+
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > navbarMenu.style.height) {
+    navbarToggler.classList.remove('active');
+    navbarMenu.classList.remove('active');
+  } else {
+    // hamburger.classList.add('active');
+    // mobile_menu.classList.add('active');
+  }
+});
+// navigation bar code end
+
 
 var slideIndex = 1;
 showSlides(slideIndex);
